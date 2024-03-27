c8c44512818591bdd5fb6f1e1d1ad2006546ec9e
SapaRandy <126409653+SapaRandy@users.noreply.github.com>
2024-03-27 17:57:26 +0100

    Add files via upload

---
+++ airneis.sql
@@ -0,0 +1,443 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 27 mars 2024 à 17:53
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `airneis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresses`
+--
+
+CREATE TABLE `adresses` (
+  `id_adresse` int(11) NOT NULL,
+  `pays` varchar(50) NOT NULL,
+  `ville` varchar(50) NOT NULL,
+  `code_postal` varchar(8) NOT NULL,
+  `rue` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresse_client`
+--
+
+CREATE TABLE `adresse_client` (
+  `id_adresse_utilisateur` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `id_adresse` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_categorie` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL,
+  `prenom` varchar(40) NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `mot_de_passe` varchar(40) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `id_adresse` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client_paiement`
+--
+
+CREATE TABLE `client_paiement` (
+  `id_utilisateur_paiement` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `id_paiement` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commandes`
+--
+
+CREATE TABLE `commandes` (
+  `id_commande` int(11) NOT NULL,
+  `reference` varchar(20) NOT NULL,
+  `prix_total` float NOT NULL,
+  `date_creation` date NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `id_etat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contenu_commande`
+--
+
+CREATE TABLE `contenu_commande` (
+  `id_contenu` int(11) NOT NULL,
+  `id_commande` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etat`
+--
+
+CREATE TABLE `etat` (
+  `id_etat` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+CREATE TABLE `image` (
+  `id_image` int(11) NOT NULL,
+  `lien` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image_produit`
+--
+
+CREATE TABLE `image_produit` (
+  `id_image_produit` int(11) NOT NULL,
+  `id_image` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiaux`
+--
+
+CREATE TABLE `materiaux` (
+  `id_materiel` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiaux_produit`
+--
+
+CREATE TABLE `materiaux_produit` (
+  `id_matiere_produit` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `id_matiere` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paiement`
+--
+
+CREATE TABLE `paiement` (
+  `id_paiement` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `numero_carte` varchar(25) NOT NULL,
+  `date_expiration` date NOT NULL,
+  `cvv` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+
+CREATE TABLE `panier` (
+  `id_panier` int(11) NOT NULL,
+  `date_creation` date NOT NULL,
+  `date_modification` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits`
+--
+
+CREATE TABLE `produits` (
+  `id_produit` int(11) NOT NULL,
+  `reference` varchar(15) NOT NULL,
+  `nom` int(50) NOT NULL,
+  `prix` float NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `quantite` int(5) NOT NULL,
+  `date_creation` date NOT NULL,
+  `marque` int(11) NOT NULL,
+  `categorie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit_panier`
+--
+
+CREATE TABLE `produit_panier` (
+  `id_panier_produit` int(11) NOT NULL,
+  `id_prouit` int(11) NOT NULL,
+  `id_panier` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `adresses`
+--
+ALTER TABLE `adresses`
+  ADD PRIMARY KEY (`id_adresse`);
+
+--
+-- Index pour la table `adresse_client`
+--
+ALTER TABLE `adresse_client`
+  ADD PRIMARY KEY (`id_adresse_utilisateur`),
+  ADD KEY `id_adresse` (`id_adresse`),
+  ADD KEY `id_utilisateur` (`id_client`);
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_categorie`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `id_adresse` (`id_adresse`);
+
+--
+-- Index pour la table `client_paiement`
+--
+ALTER TABLE `client_paiement`
+  ADD PRIMARY KEY (`id_utilisateur_paiement`),
+  ADD KEY `id_utilisateur` (`id_client`),
+  ADD KEY `id_paiement` (`id_paiement`);
+
+--
+-- Index pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`id_commande`),
+  ADD UNIQUE KEY `reference` (`reference`),
+  ADD KEY `id_client` (`id_client`),
+  ADD KEY `id_etat` (`id_etat`);
+
+--
+-- Index pour la table `contenu_commande`
+--
+ALTER TABLE `contenu_commande`
+  ADD PRIMARY KEY (`id_contenu`),
+  ADD KEY `id_commande` (`id_commande`),
+  ADD KEY `id_produit` (`id_produit`);
+
+--
+-- Index pour la table `etat`
+--
+ALTER TABLE `etat`
+  ADD PRIMARY KEY (`id_etat`);
+
+--
+-- Index pour la table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id_image`);
+
+--
+-- Index pour la table `image_produit`
+--
+ALTER TABLE `image_produit`
+  ADD PRIMARY KEY (`id_image_produit`),
+  ADD UNIQUE KEY `id_image` (`id_image`),
+  ADD KEY `id_produit` (`id_produit`);
+
+--
+-- Index pour la table `materiaux`
+--
+ALTER TABLE `materiaux`
+  ADD PRIMARY KEY (`id_materiel`);
+
+--
+-- Index pour la table `materiaux_produit`
+--
+ALTER TABLE `materiaux_produit`
+  ADD PRIMARY KEY (`id_matiere_produit`),
+  ADD KEY `id_matiere` (`id_matiere`),
+  ADD KEY `id_produit` (`id_produit`);
+
+--
+-- Index pour la table `paiement`
+--
+ALTER TABLE `paiement`
+  ADD PRIMARY KEY (`id_paiement`);
+
+--
+-- Index pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD PRIMARY KEY (`id_panier`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD KEY `categorie` (`categorie`);
+
+--
+-- Index pour la table `produit_panier`
+--
+ALTER TABLE `produit_panier`
+  ADD PRIMARY KEY (`id_panier_produit`),
+  ADD KEY `id_panier` (`id_panier`),
+  ADD KEY `id_prouit` (`id_prouit`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adresses`
+--
+ALTER TABLE `adresses`
+  MODIFY `id_adresse` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `adresse_client`
+--
+ALTER TABLE `adresse_client`
+  MODIFY `id_adresse_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client_paiement`
+--
+ALTER TABLE `client_paiement`
+  MODIFY `id_utilisateur_paiement` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contenu_commande`
+--
+ALTER TABLE `contenu_commande`
+  MODIFY `id_contenu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `etat`
+--
+ALTER TABLE `etat`
+  MODIFY `id_etat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `image_produit`
+--
+ALTER TABLE `image_produit`
+  MODIFY `id_image_produit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `materiaux`
+--
+ALTER TABLE `materiaux`
+  MODIFY `id_materiel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `materiaux_produit`
+--
+ALTER TABLE `materiaux_produit`
+  MODIFY `id_matiere_produit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `paiement`
+--
+ALTER TABLE `paiement`
+  MODIFY `id_paiement` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `panier`
+--
+ALTER TABLE `panier`
+  MODIFY `id_panier` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `produit_panier`
+--
+ALTER TABLE `produit_panier`
+  MODIFY `id_panier_produit` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
